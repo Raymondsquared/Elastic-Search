bc6f57c0c3b6020b47d81f05ba5b994075f6457c
raymondsquared <raymond_squared@live.com>
2017-01-05 18:52:43 +1100

    Add multiple records

--- put.sh
+++ put.sh
@@ -1,11 +1,30 @@
 #!/bin/sh -e
 
 # Add a single record
-curl -XPUT $RR_AWS_ELASTIC_SEARCH_URL'/megacorp/employee/1?pretty' -d'
+# curl -XPUT $RR_AWS_ELASTIC_SEARCH_URL'/megacorp/employee/1?pretty' -d'
+# {
+#     "first_name" : "John",
+#     "last_name" :  "Smith",
+#     "age" :        25,
+#     "about" :      "I love to go rock climbing",
+#     "interests": [ "sports", "music" ]
+# }'
+
+# Add multiple records
+curl -XPUT $RR_AWS_ELASTIC_SEARCH_URL'/megacorp/employee/2?pretty' -d'
 {
-    "first_name" : "John",
-    "last_name" :  "Smith",
-    "age" :        25,
-    "about" :      "I love to go rock climbing",
-    "interests": [ "sports", "music" ]
+    "first_name" :  "Jane",
+    "last_name" :   "Smith",
+    "age" :         32,
+    "about" :       "I like to collect rock albums",
+    "interests":  [ "music" ]
+}'
+
+curl -XPUT $RR_AWS_ELASTIC_SEARCH_URL'/megacorp/employee/3?pretty' -d'
+{
+    "first_name" :  "Douglas",
+    "last_name" :   "Fir",
+    "age" :         35,
+    "about":        "I like to build cabinets",
+    "interests":  [ "forestry" ]
 }'
