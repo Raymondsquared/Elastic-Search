c3ec064cbb22ff14c64e4104ee862dc1c672f19e
raymondsquared <raymond_squared@live.com>
2017-01-05 19:08:30 +1100

    Retreiving a document

--- get.sh
+++ get.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -e
 
 # Count the number of documents in the cluster
-curl -XGET $RR_AWS_ELASTIC_SEARCH_URL'/_count?pretty' -d '
-{
-    "query": {
-        "match_all": {}
-    }
-}
-'
+# curl -XGET $RR_AWS_ELASTIC_SEARCH_URL'/_count?pretty' -d '
+# {
+#     "query": {
+#         "match_all": {}
+#     }
+# }
+# '
+
+# Retreiving a document, specify the address of the document—the index, type, and ID. 
+curl -XGET $RR_AWS_ELASTIC_SEARCH_URL'/megacorp/employee/1?pretty'
